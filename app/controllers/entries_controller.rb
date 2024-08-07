4d0877f14512248d28390722a4426a48eaa7e195
EliaGiangio <eliagiang@yahoo.com>
2024-08-07 11:22:10 +0200

    Completed the logic with controllers and views

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,33 @@
 class EntriesController < ApplicationController
+    # Initialize a new Entry object for the form at launch
   def index
+    @entry = Entry.new
   end
-
+  #when a form is submitted "create" handles the comunnication with the API
   def create
+    # location extracted from the form parameters
+    location = params[:entry][:location]
+
+    # this block sends "location" data to the API and fetches the response
+    weather_data = WeatherApi.fetch_weather(location)
+    weather_main = weather_data[:weather_main]
+    loc_name = weather_data[:loc_name]
+    weather_desc = weather_data[:weather_desc]
+    temp = weather_data[:temp]
+
+    # Create a new entry
+    @entry = Entry.new(city: loc_name, weather: weather_main, description: weather_desc, temp: temp)
+
+    respond_to do |format|
+      if @entry.save
+        # If the entry is successfully saved, redirect to the entries path.
+        format.html { redirect_to entries_path }
+        format.turbo_stream
+      else        
+        # If the entry fails to save, render the index template again with an error
+        format.html { render :index, status: :unprocessable_entity }
+        format.turbo_stream
+      end
+    end
   end
 end
--- app/services/weather_api.rb
+++ app/services/weather_api.rb
@@ -3,13 +3,16 @@ class WeatherApi
   require 'json'
   require 'uri'
 
-  # We use this method to retrieve data for a given city with OpenWeatherApi
-  def self.fetch_weather(city)
+  # We use this method to retrieve data for a given location with OpenWeatherApi
+  def self.fetch_weather(location)
     api_key = ENV['OPENWEATHER_API_KEY']
-    url = "https://api.openweathermap.org/data/2.5/weather?q=#{URI.encode_www_form_component(city)}&appid=#{api_key}"
+    url = "https://api.openweathermap.org/data/2.5/weather?q=#{URI.encode_www_form_component(location)}&appid=#{api_key}"
     uri = URI(url)
     response = Net::HTTP.get(uri)
     data = JSON.parse(response)
+
+    #Very basic and NOT ideal error handling logic, if there is an unexpected api response we feed the variables "No data available"
+    if data['weather']
 
     # Data estracted from the API response
     loc_name = data['name']
@@ -24,5 +27,17 @@ class WeatherApi
       weather_desc: weather_desc,
       temp: temp
     }
+  else
+    loc_name = 'No data available'
+    weather_main = 'No data available'
+    weather_desc = 'No data available'
+    temp = 'No data available'
+    {
+      weather_main: weather_main,
+      loc_name: loc_name,
+      weather_desc: weather_desc,
+      temp: temp
+    }
+  end
   end
 end
